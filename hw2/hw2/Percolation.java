f29fde4177ea34b2192341ad6d63030c16557f27
o0bailong0o <lib@allegheny.edu>
2020-08-25 22:44:29 -0400

    first commit

---
+++ hw2/hw2/Percolation.java
@@ -0,0 +1,89 @@
+package hw2;
+
+import edu.princeton.cs.algs4.WeightedQuickUnionUF;
+
+public class Percolation {
+    private boolean[][] grid;
+    private int size;
+    private int top;
+    private int bottom;
+    private WeightedQuickUnionUF uf;
+    private WeightedQuickUnionUF ufExcludeBottom; // to avoid backwash
+    private int numOpenSites = 0;
+    private int[][] surroundings = new int[][]{{0, 1}, {0, -1}, {1, 0}, {-1, 0}};
+
+    public Percolation(int N) {
+        if (N <= 0) {
+            throw new IllegalArgumentException();
+        }
+        grid = new boolean[N][N];
+        size = N;
+        top = 0; // virtual top node
+        bottom = N * N + 1; // virtual bottom node
+        uf = new WeightedQuickUnionUF(N * N + 2);
+        ufExcludeBottom = new WeightedQuickUnionUF(N * N + 1);
+    }
+
+    private int xyTo1D(int row, int col) {
+        return row * size + col + 1;
+    }
+
+    private void validate(int row, int col) {
+        if (row < 0 || col < 0 || row >= size || col >= size) {
+            throw new IndexOutOfBoundsException();
+        }
+    }
+    public void open(int row, int col) {
+        validate(row, col);
+        if (!isOpen(row, col)) {
+            grid[row][col] = true;
+            numOpenSites += 1;
+        }
+        if (row == 0) {
+            uf.union(xyTo1D(row, col), top);
+            ufExcludeBottom.union(xyTo1D(row, col), top);
+        }
+        if (row == size - 1) {
+            uf.union(xyTo1D(row, col), bottom);
+        }
+        for (int[] surrounding : surroundings) {
+            int adjacentRow = row + surrounding[0];
+            int adjacentCol = col + surrounding[1];
+            if (adjacentRow >= 0 && adjacentRow < size) {
+                if (adjacentCol >= 0 && adjacentCol < size) {
+                    if (isOpen(adjacentRow, adjacentCol)) {
+                        uf.union(xyTo1D(row, col), xyTo1D(adjacentRow, adjacentCol));
+                        ufExcludeBottom.union(xyTo1D(row, col), xyTo1D(adjacentRow, adjacentCol));
+                    }
+                }
+            }
+        }
+    }
+    public boolean isOpen(int row, int col) {
+        validate(row, col);
+        return grid[row][col];
+    }
+
+    // is the site (row, col) full?
+    public boolean isFull(int row, int col) {
+        validate(row, col);
+        return ufExcludeBottom.connected(xyTo1D(row, col), top);
+    }
+
+    // number of open sites
+    public int numberOfOpenSites() {
+        return numOpenSites;
+    }
+
+    // does the system percolate?
+    public boolean percolates() {
+        return uf.connected(top, bottom);
+    }
+
+    // use for unit testing (not required, but keep this here for the autograder)
+    public static void main(String[] args) {
+
+    }
+
+
+}
---
+++ lab7/BSTMap.java
@@ -0,0 +1,116 @@
+import java.util.HashSet;
+import java.util.Set;
+import java.util.Iterator;
+import java.util.Stack;
+
+public class BSTMap<K extends Comparable<K>, V> implements Map61B<K, V> {
+    private Node root;
+
+    private class Node {
+        private K key;
+        private V value;
+        private Node left, right;
+        private int size;
+
+        public Node(K key, V val, int size) {
+            this.key = key;
+            this.value = val;
+            this.size = size;
+        }
+    }
+
+
+    @Override
+    public void clear() {
+        root = null;
+
+    }
+
+    @Override
+    public boolean containsKey(K key) {
+        if (key == null) {
+            throw new IllegalArgumentException();
+        }
+        return get(key) != null;
+    }
+
+    @Override
+    public V get(K key) {
+        return get(root, key);
+    }
+
+    private V get(Node x, K key) {
+        if (key == null) {
+            throw new IllegalArgumentException();
+        }
+        if (x == null) {
+            return null;
+        }
+        int cmp = key.compareTo(x.key);
+        if (cmp < 0) {
+            return get(x.left, key);
+        } else if (cmp > 0) {
+            return get(x.right, key);
+        } else {
+            return x.value;
+        }
+    }
+
+    @Override
+    public int size() {
+        return size(root);
+    }
+
+    private int size(Node x) {
+        if (x == null) {
+            return 0;
+        } else {
+            return x.size;
+        }
+    }
+
+    @Override
+    public void put(K key, V value) {
+        if (key == null) {
+            throw new IllegalArgumentException();
+        }
+        root = put(root, key, value);
+    }
+
+    private Node put(Node x, K key, V value) {
+        if (x == null) {
+            return new Node(key, value, 1);
+        }
+        int cmp = key.compareTo(x.key);
+        if (cmp < 0) {
+            x.left = put(x.left, key, value);
+        } else if (cmp > 0) {
+            x.right = put(x.right, key, value);
+        } else {
+            x.value = value;
+        }
+        x.size = 1 + size(x.left) + size(x.right);
+        return x;
+    }
+
+
+    @Override
+    public Set<K> keySet() {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public V remove(K key) {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public V remove(K key, V value) {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public Iterator<K> iterator() {
+        throw new UnsupportedOperationException();
+    }
+}
